00777cee8379e379729fbdcde1a59be9cce9dc08
FatmaMagdy <34173336+FatmaMagdy@users.noreply.github.com>
2020-01-10 13:10:13 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author fatmahammam
+ *
+ */
+public class HelloWorld{
+	public static void main(String[] args) {
+	// Print in the terminal window
+	System.out.println("Hello, world Fatma is here");
+	}
+}
